3a02cc6e38723dca195d0adfae17e4abbc2b6537
Apurva Raj <apurvarajoff@gmail.com>
2026-01-13 12:07:32 +0530

    first commit

---
+++ src/app/shared/control/control.component.ts
@@ -0,0 +1,17 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+import { Input } from '@angular/core';
+
+@Component({
+  selector: 'app-control',
+  standalone: true,
+  imports: [],
+  templateUrl: './control.component.html',
+  styleUrl: './control.component.css',
+  encapsulation: ViewEncapsulation.None,
+  host :{
+    class: 'control'
+  }
+})
+export class ControlComponent {
+  @Input({required:true}) label :string | undefined;
+}
